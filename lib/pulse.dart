5dfe2beda2baa545bb1651f0ee26bc56db4fc20e
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-14 14:40:16 +0100

    First commit

---
+++ lib/pulse.dart
@@ -0,0 +1,12 @@
+library pulse;
+
+import 'package:flutter/widgets.dart';
+
+class Pulse extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      
+    );
+  }
+}
