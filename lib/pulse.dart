2315ff3d3066f2a17471b090ad405a65f6967f50
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-15 17:28:53 +0100

    Added PulsePaint

--- lib/pulse.dart
+++ lib/pulse.dart
@@ -6,7 +6,24 @@ class Pulse extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Opacity(
+class PulsePaint extends CustomPainter {
+  final double radius;
+  final Color color;
+  final Offset offset;
+
+  PulsePaint({
+    this.radius,
+    this.color,
+    this.offset,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final _paint = Paint()..color = color;
       
-    );
+    canvas.drawCircle(offset, radius, _paint);
   }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
